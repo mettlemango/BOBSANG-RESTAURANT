fb582d2d2d0baebd8e83291a6dc3e93c4a99ff66
Courage Genito <courage_genito@dlsu.edu.ph>
2024-04-04 22:55:16 +0800

    hatdog

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     var password = document.getElementById("password").value;
 
     // Your authentication logic goes here
-    if (username === "admin" && password === "adwmin123") {
+    if (username === "admin" && password === "admin123") {
         // Redirect to reports page or perform other actions
         window.location.href = "reports.html";
     } else {
         document.getElementById("error-message").innerText = "Invalid username or password";
     }
 });
+
+function increaseQuantity(item) {
+    var quantityInput = document.getElementById(item + 'Quantity');
+    var value = parseInt(quantityInput.value);
+    if (value < parseInt(quantityInput.max)) {
+        value++;
+        quantityInput.value = value;
+    }
+}
+
+function decreaseQuantity(item) {
+    var quantityInput = document.getElementById(item + 'Quantity');
+    var value = parseInt(quantityInput.value);
+    if (value > parseInt(quantityInput.min)) {
+        value--;
+        quantityInput.value = value;
+    }
+}
+
+
+
+
