6e648c97cdc6accd939305cadb3d61f670645eb6
danapelagio22 <dana_pelagio@dlsu.edu.ph>
2024-04-14 12:04:04 +0800

    //

--- dippings.php
+++ dippings.php
@@ -19,8 +19,10 @@
 
     <div class="div2">
         <h1 class="headerIntro">Dippings</h1>
-        <h2 class="headerIntro">딥핑 소스</h1>
+        <h2 class="header2Intro">딥핑 소스</h2>
     </div>
+
+    
 
     <div class="containerDineIn">
         <!-- Cheese Dip Section -->
